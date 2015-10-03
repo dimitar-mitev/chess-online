3cfc3999471226d19813863d5c4ec77bd5a4892c
unknown <erty.mitev@gmail.com>
2015-10-03 15:41:42 +0300

    initial projects

---
+++ source/Web/chess-online.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(chess_online.Web.Startup))]
+namespace chess_online.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
